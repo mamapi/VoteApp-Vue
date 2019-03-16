b085b57a83b5d01a7f79629d553cc820eb3d422b
grzegorzg <grzegorz@devstream.pl>
2019-03-16 10:28:45 +0100

    [#1.1] Added pages: Home, Vote, Score, Settings

--- src/router.js
+++ src/router.js
@@ -1,6 +1,9 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
+import Score from './views/Score.vue'
+import Vote from './views/Vote.vue'
+import Settings from './views/Settings.vue'
 
 Vue.use(Router)
 
@@ -14,12 +17,19 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/vote',
+      name: 'vote',
+      component: Vote
+    },
+    {
+      path: '/score',
+      name: 'score',
+      component: Score
+    },
+    {
+      path: '/settings',
+      name: 'settings',
+      component: Settings
     }
   ]
 })
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld />
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+
+//1/ 1. In vue project @ is an alias to /src.
+import HelloWorld from '@/components/HelloWorld.vue';
 
 export default {
+  //2/ 3. Name is not required, but it's usually good to add it
+  //info https://vuejs.org/v2/api/#name
   name: 'home',
   components: {
-    HelloWorld
-  }
-}
+    HelloWorld,
+  },
+};
 </script>
+
+<style lang="scss">
+.home {
+  //1/ 2. You can use absolute paths in styles, but remember about tilde as a prefix
+  background-repeat: no-repeat;
+  background-position: center;
+  height: 192px;
+}
+</style>
